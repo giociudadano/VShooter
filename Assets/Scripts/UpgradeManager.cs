954912f0dabdb6b077f90d4cc3c7ed29162e1f39
Gio Ciudadano <gcciudadano@gmail.com>
2024-04-21 15:40:09 +0800

    Add functionality to render upgrades

--- Assets/Scripts/UpgradeManager.cs
+++ Assets/Scripts/UpgradeManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using TMPro;
+using System;
 
 public class UpgradeManager : MonoBehaviour {
 	[SerializeField] private GameObject upgradeUI;
@@ -10,14 +11,26 @@ public class UpgradeManager : MonoBehaviour {
   public Dictionary<string, Dictionary<string, dynamic>> upgrades = new Dictionary<string, Dictionary<string, dynamic>>() {
 		{"MORICALLIOPE_SOULHARVESTER", new Dictionary<string, dynamic>() {
 			{"title", "Soul Harvester"},
-			{"description", "Killing an enemy has a 30% chance to restore 2HP."},
+			{"description", "Defeating an enemy has a {HEAL_CHANCE} chance to restore {HEAL_AMOUNT}."},
 			{"type", "Character Passive"},
-			{"level", 1},
+			{"icon", "Calliope_SoulHarvester"},
 			{"parameters", new Dictionary<string, dynamic> () {
-				{"HEAL_CHANCE", 0.3},
-				{"HEAL_AMOUNT", 2}
+				{"HEAL_CHANCE", new Dictionary<string, dynamic> () {
+					{"color", "#AFF"},
+					{"format", "0:0%"},
+					{"level", new Dictionary<string, float> () {
+						{"1", 0.2f}
+					}}
+				}},
+				{"HEAL_AMOUNT", new Dictionary<string, dynamic> () {
+					{"color", "#AFA"},
+					{"suffix", "HP"},
+					{"level", new Dictionary<string, float> () {
+						{"1", 2f}
+					}}
+				}}
 			}}
-		}}
+		}},
 	};
 
   void Start(){
@@ -30,16 +43,53 @@ public class UpgradeManager : MonoBehaviour {
 
 	public void GetUpgradePool() {
 		for (int i = 0; i < 4; i++) {
-			int upgradeIndex = Random.Range(0, upgrades.Count);
-			RenderUpgrade(i+1, upgrades.ElementAt(upgradeIndex).Value);
+			int upgradeIndex = UnityEngine.Random.Range(0, upgrades.Count);
+			RenderUpgrade(i+1, upgrades.ElementAt(upgradeIndex).Key, upgrades.ElementAt(upgradeIndex).Value);
 		}      
 	}
 
-	private void RenderUpgrade(int slotIndex, Dictionary<string, dynamic> upgrade) {
+	private void RenderUpgrade(int slotIndex, string name, Dictionary<string, dynamic> upgrade) {
 		GameObject upgradeTitleUI = upgradeUI.transform.Find("Upgrade Card " + slotIndex.ToString() + "/Upgrade Title").gameObject;
-		upgradeTitleUI.GetComponent<TMP_Text>().text = $"{upgrade["title"]} <color=#FFA>LV {upgrade["level"]}</color>";
+		upgradeTitleUI.GetComponent<TMP_Text>().text = $"{upgrade["title"]} <color=#AAA>LV {GetUpgradeLevel(name)}</color>";
 		GameObject upgradeDescriptionUI = upgradeUI.transform.Find("Upgrade Card " + slotIndex.ToString() + "/Upgrade Description").gameObject;
-		upgradeDescriptionUI.GetComponent<TMP_Text>().text = $"{upgrade["description"]}";
+		upgradeDescriptionUI.GetComponent<TMP_Text>().text = ParseAbilityDescription(name, $"{upgrade["description"]}", upgrade["parameters"]);
+		GameObject upgradeTypeUI = upgradeUI.transform.Find("Upgrade Card " + slotIndex.ToString() + "/Upgrade Type").gameObject;
+		upgradeTypeUI.GetComponent<TMP_Text>().text = ParseAbilityType(upgrade["type"]);
+		if (upgrade.ContainsKey("icon")){
+			GameObject upgradeImageUI = upgradeUI.transform.Find("Upgrade Card " + slotIndex.ToString() + "/Upgrade Image").gameObject;
+			upgradeImageUI.GetComponent<UnityEngine.UI.Image>().sprite = Resources.Load<Sprite>($"Abilities/{upgrade["icon"]}");
+		}
+	}
+
+	private string ParseAbilityType(string type) {
+		switch (type) {
+			case "Character Passive":
+				return "<color=#FFA>" + type + "</color>";
+			default:
+			  return "";
+		}
+	}
+
+	private string ParseAbilityDescription(string name, string description, Dictionary<string, dynamic> parameters) {
+	  foreach (var parameter in parameters) {
+			string query = "{" + parameter.Key + "}";
+			string result = $"{parameter.Value["level"][$"{GetUpgradeLevel(name)}"]}";
+			if (parameter.Value.ContainsKey("format")) {
+				result = String.Format("{" + parameter.Value["format"] + "}", float.Parse(result));
+			}
+			if (parameter.Value.ContainsKey("suffix")) {
+				result += parameter.Value["suffix"];
+			}
+			if (parameter.Value.ContainsKey("color")) {
+				result = $"<color={parameter.Value["color"]}>" + result + "</color>";
+			}
+			description = description.Replace(query, result);
+	  }
+	  return description;
+	}
+
+	private int GetUpgradeLevel(String name) {
+		return 1;
 	}
 
 }
