5b8174aa94e61971684e92670e40ea09eae7aaa7
Gio Ciudadano <gcciudadano@gmail.com>
2024-04-30 20:55:09 +0800

    Add on player heal popup text

--- Assets/Scripts/PopupTextManager.cs
+++ Assets/Scripts/PopupTextManager.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DamageTextManager : MonoBehaviour {
+public class PopupTextManager : MonoBehaviour {
 
 	[SerializeField] private float xOffset = 0.75f;
+
+  [SerializeField] private float yOffset = 0.75f;
 	
 	[SerializeField] private float textDuration = 1.25f;
 
-	float newPositionX;
+	float newPositionX, newPositionY;
 
   void Start() {
     newPositionX = transform.position.x + Random.Range(-xOffset, xOffset);
+    newPositionY = transform.position.y + yOffset;
   }
   void Update() {
-		transform.position = Vector3.Lerp(transform.position, new Vector3(newPositionX, 2.75f, transform.position.z), 0.5f);
+		transform.position = Vector3.Lerp(transform.position, new Vector3(newPositionX, newPositionY, transform.position.z), 0.5f);
     Destroy(gameObject, textDuration);
   }
 }
