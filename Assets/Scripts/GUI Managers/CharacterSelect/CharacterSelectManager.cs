7aaebb8d95f4cbf8eea275a81b51a93b3081146f
Gio Ciudadano <gcciudadano@gmail.com>
2024-05-04 22:17:40 +0800

    Add loading system for selecting different characters

--- Assets/Scripts/GUI Managers/CharacterSelect/CharacterSelectManager.cs
+++ Assets/Scripts/GUI Managers/CharacterSelect/CharacterSelectManager.cs
@@ -8,7 +8,6 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CharacterSelectManager : MonoBehaviour {
-
     [SerializeField] private GameObject header;
     [SerializeField] private GameObject characterSplash;
 
@@ -17,13 +16,17 @@ public class CharacterSelectManager : MonoBehaviour {
 
     [SerializeField] private GameObject playButton;
 
+    [SerializeField] private GameObject characterData;
+
     public bool isCharacterSelected = false;
+
 
     void Start() {
       header.gameObject.transform.localPosition = new Vector3(1200, 210, 0);
       characterSplash.gameObject.transform.localPosition = new Vector3(-700, -160, 0);
       characterNameObject.GetComponent<TMP_Text>().text = "";
       characterDescriptionObject.GetComponent<TMP_Text>().text = "";
+
       StartCoroutine(SceneStart());
     }
 
@@ -85,17 +88,25 @@ public class CharacterSelectManager : MonoBehaviour {
           characterNameObject.GetComponent<TMP_Text>().text = "MORI CALLIOPE";
           characterDescriptionObject.GetComponent<TMP_Text>().text = GetCharacterDescription(characterName);
           break;
+        case "NinomaeInanis":
+          characterSplash.transform.Find("Background").GetComponent<UnityEngine.UI.Image>().color = new Color((float)126/255, (float)66/255, (float) 195/255, 1);
+          characterNameObject.GetComponent<TMP_Text>().text = "NINOMAE INA'NIS";
+          characterDescriptionObject.GetComponent<TMP_Text>().text = GetCharacterDescription(characterName);
+          break;
       }
       characterSplash.transform.Find("Foreground").gameObject.SetActive(true);
       characterSplash.transform.Find("Foreground").GetComponent<UnityEngine.UI.Image>().sprite = splashArt;
       StopAllCoroutines();
       StartCoroutine(HoverCharacter());
+      gameObject.GetComponent<CharacterData>().selectedCharacter = characterName;
     }
 
     private string GetCharacterDescription(string characterName){
       switch (characterName) {
         case "MoriCalliope":
-          return "As the Grim Reaper's first apprentice, Mori Calliope specializes in using <color=#FFA>lifesteal</color> and <color=#FFA>post-death effects</color> to gain an advantage in the battlefield.";
+          return "Mori Calliope is the Grim Reaper's first apprentice. As death's assistant, she specializes in using <color=#FFA>lifesteal</color> and <color=#FFA>post-death effects</color> to gain an advantage in the battlefield.";
+        case "NinomaeInanis":
+          return "Ninomae Ina'nis is a priestess and worshipper to an ancient tentacle god. As a summoner, she specializes in <color=#FFA>summoning allies</color> and <color=#FFA>controlling enemies</color>.";
         default:
           return "";
       }
