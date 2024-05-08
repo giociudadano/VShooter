e9a6d4f13037a247df6eae1a1dde5c54f0b4a467
SkyBluePenguin53 <rgnaragdao@gmail.com>
2024-05-08 15:11:36 +0800

    Added healing on Mori's first skill

---
+++ Assets/Scripts/MoriCalliopeWeapon/MoriSkillAOEHeal.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoriSkillAOEHeal : MonoBehaviour
+{
+    [SerializeField] private float areaSize = 15.0f;
+    [SerializeField] private float healAmount = 10.0f;
+    [SerializeField] private float maxHealing = 60.0f;
+    private GameObject player;
+    private float counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        AOEHealing(healAmount);
+    }
+
+    // Update is called once per frame
+    private void AOEHealing(float healAmount){
+	  var colliders = Physics.OverlapSphere(player.transform.position, areaSize);
+      foreach (var col in colliders){
+          if (col.GetComponent<Collider>().CompareTag("Enemy")){
+            counter++;
+          };
+      };
+      healAmount *= counter;
+      healAmount = Math.Min(healAmount, maxHealing);
+      if(healAmount > 0)
+      {
+        player.GetComponent<PlayerHealthManager>().Heal(healAmount);
+      }
+	}
+}
