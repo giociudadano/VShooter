fb1f06d614ee7ed598be9bbc56a4efc76a3197c2
RB <rbjocsing@up.edu.ph>
2024-05-27 09:45:57 +0800

    Implemented `Restart` to `CharacterSelect`

--- Assets/Scripts/Managers/RestartGame.cs
+++ Assets/Scripts/Managers/RestartGame.cs
@@ -5,14 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class RestartGame : MonoBehaviour {
 
+  private static List<GameObject> persistentObjects = new List<GameObject>();
     void Start() {
         
     }
     void Update() {
         
     }
-    public void ReloadScene() {
+
+    //  Hackish method to destroy all DontDestroyOnLoadObjects
+    private void DestroyPersistentObjects() {
+      var sacrifice = new GameObject("Sacrificial Lamb");
+      DontDestroyOnLoad(sacrifice);
+
+      foreach(var root in sacrifice.scene.GetRootGameObjects()) {
+        Destroy(root);
+      }
+    }
+    public void ReloadScene()
+    {
       Time.timeScale = 1;
       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Restart()
+    {
+      Time.timeScale = 1;
+      DestroyPersistentObjects();
+      SceneManager.LoadScene("CharacterSelect");
+
     }
 }
