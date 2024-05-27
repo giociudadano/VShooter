3d36ee8c371220c8b37b9d20c269f11c833e39e4
RB <rbjocsing@up.edu.ph>
2024-05-27 10:08:30 +0800

    Removed obsolete `DestroyParticleAfterDelay` from explosion particles

--- Assets/Scripts/Effects/DestroyParticleAfterDelay.cs
+++ Assets/Scripts/Effects/DestroyParticleAfterDelay.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Obsolete]  //  Now destroying particles using native Unity particle system settings
 public class DestroyParticleAfterDelay : MonoBehaviour
 {
     // Start is called before the first frame update
